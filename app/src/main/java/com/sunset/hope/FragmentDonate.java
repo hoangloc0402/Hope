ed6e46920ad88d5960629d5b6a56a0db9c99dd14
Hoang Loc <hoangloc0402@gmail.com>
2019-02-23 17:08:07 +0700

    add recycler view

--- app/src/main/java/com/sunset/hope/FragmentDonate.java
+++ app/src/main/java/com/sunset/hope/FragmentDonate.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentDonate extends Fragment {
     View myView;
+    RecyclerView recyclerView;
+    AdapterRCVPost adapterRCV;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -19,6 +24,13 @@ public class FragmentDonate extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView =  inflater.inflate(R.layout.fragment_donate, container, false);
+        recyclerView = myView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+        adapterRCV= new AdapterRCVPost(null);
+        recyclerView.setAdapter(adapterRCV);
+     
         return myView;
     }
 }
