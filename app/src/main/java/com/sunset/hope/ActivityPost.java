24b1f8e0e56fcfccfa38f4362de246e7392504c8
Van Minh Hào <1510901@hcmut.edu.vn>
2019-02-23 22:29:53 +0700

    Add ActivityPost, "UserInfo" -> "Contact"

--- app/src/main/java/com/sunset/hope/ActivityPost.java
+++ app/src/main/java/com/sunset/hope/ActivityPost.java
@@ -3,8 +3,9 @@ package com.sunset.hope;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -25,11 +27,29 @@ public class ActivityPost extends AppCompatActivity {
     private EditText titleInput = null;
     private EditText descriptionInput = null;
     private Button postButton =  null;
-    private TextView emailNoti = null;
+    private TextView notiText = null;
     private Spinner typeInput = null;
     private Spinner tagsInput = null;
+    private static LayoutInflater inflater;
+    private static View view;
 
     private static Pattern emailRegexPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$", Pattern.CASE_INSENSITIVE);
+
+    public void setTypeSpinnerElements(){
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), Types.getTypeList().icon, Types.getTypeList().text);
+        typeInput.setAdapter(customAdapter);
+    }
+
+    public void setTagSpinnerElements(){
+        List<String> categories = new ArrayList<String>();
+        categories.add("Select Tag");
+        categories.add("Donate");
+        categories.add("Request");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tagsInput.setAdapter(dataAdapter);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,21 +64,12 @@ public class ActivityPost extends AppCompatActivity {
         titleInput = findViewById(R.id.txt_title);
         descriptionInput = findViewById(R.id.txt_description);
         postButton = findViewById(R.id.btn_post);
-        emailNoti = findViewById(R.id.txtview_email_noti);
+        notiText = findViewById(R.id.txtview_noti);
         typeInput = findViewById(R.id.spn_type);
         tagsInput = findViewById(R.id.spn_tags);
 
-        List<String> categories = new ArrayList<String>();
-        categories.add("Select Type");
-        categories.add("Money");
-        categories.add("Bloods");
-        categories.add("Clothes");
-        categories.add("Foods");
-        categories.add("Medicines");
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        typeInput.setAdapter(dataAdapter);
+        setTypeSpinnerElements();
+        setTagSpinnerElements();
 
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +77,10 @@ public class ActivityPost extends AppCompatActivity {
                 String emailString = emailInput.getText().toString();
 
                 if (!checkInfo(emailString)) {
-                    emailNoti.setText("Wrong email");
+                    notiText.setText("Wrong email");
+                }
+                else if ((typeInput.getSelectedItemId() == 0) || (tagsInput.getSelectedItemId() == 0)) {
+                    notiText.setText("Wrong type or tag");
                 }
                 else {
                     String phoneNumberString = phoneNumberInput.getText().toString();
@@ -76,6 +90,9 @@ public class ActivityPost extends AppCompatActivity {
                     String typeString = typeInput.getSelectedItem().toString();
                     String tagsString = tagsInput.getSelectedItem().toString();
 
+                    Log.d("type_get", typeString);
+                    Log.d("tag_get", tagsString);
+
                     Contact contact = new Contact(phoneNumberString, emailString, addressString);
 //                    Post post = new Post("P111111", "U111111", typeString, new String[]{"", ""}, new int[]{0,0}, descriptionString, false, 0, titleString, contact);
 
@@ -84,8 +101,6 @@ public class ActivityPost extends AppCompatActivity {
             }
         });
     }
-
-
 
     public boolean checkInfo(String emailString){
         return (emailRegexPattern.matcher(emailString).matches());
---
+++ app/src/main/java/com/sunset/hope/Types.java
@@ -0,0 +1,31 @@
+package com.sunset.hope;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Types {
+    int[] icon;
+    String[] text;
+
+    public Types(int[] icon, String[] text){
+        this.icon = icon;
+        this.text = text;
+    }
+
+    public static Types getTypeList(){
+        Types type = new Types(new int[]{R.drawable.spn_drop_down,
+                R.drawable.spn_bloods,
+                R.drawable.spn_clothes,
+                R.drawable.spn_foods,
+                R.drawable.spn_medicines,
+                R.drawable.spn_money},
+                new String[]{"Select Tag",
+                        "Bloods",
+                        "Clothes",
+                        "Foods",
+                        "Medicines",
+                        "Money"});
+        return type;
+    }
+}
