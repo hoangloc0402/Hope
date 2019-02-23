2b6e757080f73a41bb2fa24fa1fe230608266a26
Hoang Loc <hoangloc0402@gmail.com>
2019-02-23 19:24:04 +0700

    add recycler view

--- app/src/main/java/com/sunset/hope/FragmentDonate.java
+++ app/src/main/java/com/sunset/hope/FragmentDonate.java
@@ -1,14 +1,21 @@
 package com.sunset.hope;
 
+import android.content.ClipData;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class FragmentDonate extends Fragment {
     View myView;
@@ -30,7 +37,20 @@ public class FragmentDonate extends Fragment {
 
         adapterRCV= new AdapterRCVPost(null);
         recyclerView.setAdapter(adapterRCV);
-     
+
+        adapterRCV.setItemClickListener((view, position) -> {
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Option")
+                    .setItems(R.array.listOption, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    }).setIcon(R.drawable.ic_call);
+            builder.create();
+            builder.show();
+        });
         return myView;
     }
 }
