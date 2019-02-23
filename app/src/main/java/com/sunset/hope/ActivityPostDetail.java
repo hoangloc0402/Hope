a056c080e70d18612a6ef4aaa69b7c5c8515839d
LampardNguyen234 <thanh230497@gmail.com>
2019-02-23 20:34:34 +0700

    Add Firebase

--- app/src/main/java/com/sunset/hope/ActivityPostDetail.java
+++ app/src/main/java/com/sunset/hope/ActivityPostDetail.java
@@ -2,12 +2,34 @@ package com.sunset.hope;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ActivityPostDetail extends AppCompatActivity {
+
+    ListView lvComment;
+    ArrayList<Comment> commentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
+
+        lvComment = findViewById(R.id.lv_post_comment);
+
+        commentList = new ArrayList<Comment>();
+
+        commentList.add(new Comment("1", "1", "Hello", "Lampard"));
+        commentList.add(new Comment("1", "2", "HelloWorld", "Lampard"));
+        commentList.add(new Comment("1", "3", "HelloYou", "Thanh"));
+        commentList.add(new Comment("1", "4", "HelloMe", "Lampard"));
+
+        AdapterPostDetail adapter = new AdapterPostDetail(ActivityPostDetail.this,
+                R.layout.comment_layout, commentList);
+
+        lvComment.setAdapter(adapter);
     }
+
+
 }
---
+++ app/src/main/java/com/sunset/hope/Firebase.java
@@ -0,0 +1,42 @@
+package com.sunset.hope;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class Firebase {
+    public static DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    public static DatabaseReference userDatabase = rootRef.child("Users");
+    public static DatabaseReference postDatabase = rootRef.child("Posts");
+
+    public static ArrayList<User> userList = new ArrayList<>();
+    public static ArrayList<String> userID = new ArrayList<>();
+
+    public static ArrayList<Post> postList = new ArrayList<>();
+    public static ArrayList<String> postID = new ArrayList<>();
+
+    public static void addNewUser(User user){
+//        rootRef.child("Users").child(user.getId()).setValue(user);
+    }
+
+    public static void addNewPost(Post post){
+//        rootRef.child("Posts").child(post.getId()).setValue(post);
+    }
+
+    public static void getAllPost() {
+
+    }
+
+
+
+
+
+}
