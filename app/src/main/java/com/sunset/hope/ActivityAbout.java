b3b1053a26dba6a659f0a82e06c05441098344cc
Hoang Loc <hoangloc0402@gmail.com>
2019-02-24 01:46:53 +0700

    1006th commit

--- app/src/main/java/com/sunset/hope/ActivityAbout.java
+++ app/src/main/java/com/sunset/hope/ActivityAbout.java
@@ -1,4 +1,13 @@
 package com.sunset.hope;
 
-public class ActivityAbout {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityAbout extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 }
