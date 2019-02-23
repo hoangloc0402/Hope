d5f4a9197f07ec3cf03f9e47e3e114fdb34b169f
Hoang Loc <hoangloc0402@gmail.com>
2019-02-24 01:17:25 +0700

    1004th commit

---
+++ app/src/main/java/com/sunset/hope/ActivityViewProfile.java
@@ -0,0 +1,13 @@
+package com.sunset.hope;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityViewProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
